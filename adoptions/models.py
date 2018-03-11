6dbf003baa7fbbe2d9b58ffb9b49430ea5c36df3
David Ornellast <ornellasd@gmail.com>
2018-03-11 12:00:01 -0700

    implements templates

--- adoptions/models.py
+++ adoptions/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Pet(models.Model):
-    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    SEX_CHOICES = [
+        ('M', 'Male'), 
+        ('F', 'Female'),
+    ]
     name = models.CharField(max_length=100)
     submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
