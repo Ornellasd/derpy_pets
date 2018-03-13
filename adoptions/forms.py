2ad0dda6d343e60dc5f0414cfecb0b3d0f48a776
David Ornellast <ornellasd@gmail.com>
2018-03-13 15:01:32 -0700

    finalizes user/pet association

--- adoptions/forms.py
+++ adoptions/forms.py
@@ -5,4 +5,4 @@ from .models import Pet
 class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
-        fields = ['name', 'submitter', 'species', 'breed', 'description', 'sex', 'age', 'vaccinations']
+        fields = ['name', 'species', 'breed', 'description', 'sex', 'age', 'vaccinations']
--- adoptions/models.py
+++ adoptions/models.py
@@ -8,7 +8,7 @@ class Pet(models.Model):
         ('F', 'Female'),
     ]
     name = models.CharField(max_length=100)
-    submitter = models.CharField(max_length=100)
+    #submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
