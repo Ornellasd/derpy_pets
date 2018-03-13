1423b088c9d17ede2f5b4821e69b51b76aa46b1d
David Ornellast <ornellasd@gmail.com>
2018-03-13 13:21:46 -0700

    initializes user login

--- derpy_pets/urls.py
+++ derpy_pets/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from adoptions import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('', views.home, name='home'),
-    path('adoptions/<int:id>/', views.pet_detail, name='pet_detail'),
     path('new_pet', views.new_pet, name='new_pet'),
+    path('adoptions/<int:id>/', views.pet_detail, name='pet_detail'),
     path('adoptions/<int:id>/edit_pet/', views.edit_pet, name='edit_pet'),
 ]
+
+
